a3bce22828379427a00e7413387d8c9ff43a3c63
Итачи <potroshitel2007@vk.com>
2024-09-30 00:08:41 +0300

    3.1 5: текстовый редактор с шифтом неправильный ответ на тесте 9(

---
+++ 3_1_containers/text_editor2.cpp
@@ -0,0 +1,160 @@
+#include <iostream>
+#include <string>
+#include <list>
+#include <utility>
+
+
+struct TextEditor {
+    std::list<std::string> document;
+    std::list<std::string>::iterator cursor;
+    std::list<std::string>::iterator secondaryCursor;
+    std::list<std::string> largeBuffer;
+    bool shiftPressed = false;
+    bool reachedBegin = false;
+    bool reversed = false;
+};
+
+
+void InitEditor(TextEditor& editor) {
+    std::string line;
+	while (true) {
+        std::getline(std::cin, line);
+        if (line.empty()) {
+            break;
+        }
+        editor.document.push_back(line);
+    }
+    editor.cursor = editor.document.begin();
+}
+
+
+void PrintDocument(const TextEditor& editor) {
+    for (const auto& s : editor.document) {
+        std::cout << s << '\n';
+    }
+}
+
+
+void AdjustCursors(TextEditor& editor) {
+    if (editor.reversed) {
+        std::swap(editor.cursor, editor.secondaryCursor);
+        editor.reversed = false;
+    } else {
+        if (editor.secondaryCursor != editor.document.end()) {
+            ++editor.secondaryCursor;
+        }
+        if (!editor.reachedBegin) {
+            ++editor.cursor;
+            editor.reachedBegin = false;
+        }
+    }
+}
+
+void Shift(TextEditor& editor) {
+	editor.shiftPressed = true;
+	editor.secondaryCursor = editor.cursor;
+}
+
+
+void Down(TextEditor& editor) {
+    editor.reachedBegin = false;
+    if (editor.shiftPressed && editor.cursor == editor.secondaryCursor) {
+        editor.reversed = true; 
+    }
+    if (editor.cursor == editor.document.end()) {
+        return;
+    }
+    ++editor.cursor;
+}
+
+
+void Up(TextEditor& editor) {
+    if (editor.shiftPressed && editor.cursor == editor.secondaryCursor) {
+        editor.reversed = false;
+    }
+    if (editor.cursor == editor.document.begin()) {
+        editor.reachedBegin = true;
+        return;
+    }
+    --editor.cursor;
+}
+
+
+void CtrlX(TextEditor& editor) {
+	if (!editor.shiftPressed && editor.cursor == editor.document.end()) {
+        return;
+    }
+    if (editor.shiftPressed
+        && editor.cursor == editor.document.end()
+        && editor.secondaryCursor == editor.document.end()) {
+        editor.shiftPressed = false;
+        return;
+    }
+    if (editor.shiftPressed && editor.cursor != editor.secondaryCursor) {
+        AdjustCursors(editor);
+        if (!editor.largeBuffer.empty()) {
+            editor.largeBuffer.clear();
+        }
+        editor.largeBuffer.splice(
+            editor.largeBuffer.begin(),
+            editor.document,
+            editor.cursor, editor.secondaryCursor
+        );
+        editor.cursor = editor.secondaryCursor;
+        editor.shiftPressed = false;
+        return;
+	}
+    editor.largeBuffer.assign(1, *editor.cursor);
+    editor.cursor = editor.document.erase(editor.cursor);
+    editor.shiftPressed = false;
+    std::cout << editor.largeBuffer.size() << '\n';
+}
+
+
+void CtrlV(TextEditor& editor) {
+    if (editor.largeBuffer.empty()) {
+        editor.shiftPressed = false;
+        editor.reachedBegin = false;
+        editor.reversed = false;
+        return;
+    }
+    if (editor.shiftPressed && editor.cursor != editor.secondaryCursor) {
+        AdjustCursors(editor);
+        editor.cursor = editor.document.erase(editor.cursor, editor.secondaryCursor);
+	}
+    editor.document.insert(editor.cursor, editor.largeBuffer.begin(), editor.largeBuffer.end());
+    editor.shiftPressed = false;
+}
+
+
+void CommandContext(const std::string& command, TextEditor& editor) {
+    if (command == "Down") {
+        Down(editor);
+    } else if (command == "Up") {
+        Up(editor);
+    } else if (command == "Ctrl+X") {
+        CtrlX(editor);
+    } else if (command == "Ctrl+V") {
+        CtrlV(editor);
+    } else if (command == "Shift") {
+        Shift(editor);
+    }
+}
+
+
+int main() {
+    TextEditor editor;
+    InitEditor(editor);
+    std::string command;
+    // while (std::cin >> command) {
+    //     CommandContext(command, editor);
+    // }
+    while (true) {
+        std::getline(std::cin, command);
+        if (command.empty()) {
+            break;
+        } 
+        CommandContext(command, editor);
+    }
+    PrintDocument(editor);
+}
